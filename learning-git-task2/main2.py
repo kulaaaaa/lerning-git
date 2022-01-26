cd3f268e6047a9690f083f8e31a2c5e508b8d915
matniawarhawks <98478979+matniawarhawks@users.noreply.github.com>
2022-01-26 19:48:31 +0100

    Update main2.py
    Fix info

--- learning-git-task2/main2.py
+++ learning-git-task2/main2.py
@@ -20,7 +20,7 @@ print("W sumie kupuję", count, "produktów")
 print("\n======================\n")
 
 
-#Zadanie 2
+#Zadanie 2 "Drugie"
 print("Liczby od 0 do 100 podzielne przez 2 podniesione do potęgi 3:")
 for x in range(101):
    if x%5 == 0 and x>0:
